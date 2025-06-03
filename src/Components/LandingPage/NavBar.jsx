84c0b5281f551bc3e5e64127da60246212237c8a
anshulsre8171 <anshulsre8171@gmail.com>
2025-06-03 10:54:39 +0530

    update NavBar

--- src/Components/LandingPage/NavBar.jsx
+++ src/Components/LandingPage/NavBar.jsx
@@ -1,12 +1,36 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState,useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom'; // Import useLocation
 import { FaHome,FaBriefcase, FaCheckCircle, FaInfoCircle, FaPhoneAlt,  FaUserShield, FaUserTie, FaUser,FaUserPlus, FaUserAlt, FaUsers,  FaSignOutAlt } from "react-icons/fa";
 
 function NavBar() {
+
+  const navRef = useRef();
+  // Close navbar on outside click
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        navRef.current &&
+        !navRef.current.contains(event.target)
+      ) {
+        const collapse = document.querySelector('.navbar-collapse');
+        if (collapse?.classList.contains('show')) {
+          collapse.classList.remove('show');
+        }}
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+
   const [data, setData] = useState("");
   const [userType, setUserType] = useState("");
   const nav = useNavigate();    
    const API_URL=import.meta.env.VITE_APP_API_URL
+   //const API="http://localhost:9000"
+
 
   useEffect(() => {
     const temData = JSON.parse(localStorage.getItem("data"));
@@ -35,11 +59,9 @@ function NavBar() {
 if (userType === "admin") {
   return (
     <>
-      <div className="row navbar_main_row mb-1 sticky-top mb-1 ps-5 pe-4">
-        <div className="col-12">
-          <nav className="navbar navbar-expand-lg navbar-light">
-            <div className="container-fluid">
-              <Link className="navbar-brand" to="#">
+      <div className="container-fluid navbar_main_row mb-1 px-0 sticky-top" ref={navRef}>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light p-0 px-3">
+              <Link className="navbar-brand" to="/">
                 <img className='nav_bar_logo' src={`${API_URL}/upload/${data.img}`} />
               </Link>
               <button
@@ -57,7 +79,7 @@ if (userType === "admin") {
                 <ul className="navbar-nav">
                   <li className="nav-item">
                     <Link className="nav-link nav_font" aria-current="page" to="/admin">
-                    <FaHome className="menu-icon" />&nbsp; Dashboard
+                    <FaHome className="menu-icon" />&nbsp;Dashboard
                     </Link>
                   </li>
                   <li className="nav-item">
@@ -77,19 +99,15 @@ if (userType === "admin") {
                   </li>
                 </ul>
               </div>
-            </div>
           </nav>
-        </div>
       </div>
     </>
   );
 } else if (userType === "seeker") {
   return (
     <>
-      <div className="row navbar_main_row mb-1 sticky-top mb-1 ps-5 pe-4">
-        <div className="col-12">
-          <nav className="navbar navbar-expand-lg navbar-light">
-            <div className="container-fluid">
+      <div className="container-fluid navbar_main_row mb-1 px-0 sticky-top" ref={navRef}> 
+          <nav className="navbar navbar-expand-lg navbar-light bg-light p-0 px-3">
               <Link className="navbar-brand" to="/">
               <img className='nav_bar_logo' src={`${API_URL}/upload/${data.img}`} />
               </Link>
@@ -113,12 +131,12 @@ if (userType === "admin") {
                   </li>
                   <li className="nav-item">
                     <Link className="nav-link nav_font" aria-current="page" to="/seeker/jobapply">
-                    <FaBriefcase className="menu-icon" />&nbsp;Apply Job
+                    <FaBriefcase className="menu-icon" />&nbsp;Apply&nbsp;Job
                     </Link>
                   </li>
                   <li className="nav-item">
                     <Link className="nav-link nav_font" aria-current="page" to="/seeker/appliedjob">
-                    <FaCheckCircle className="menu-icon" />&nbsp;Applied Job
+                    <FaCheckCircle className="menu-icon" />&nbsp;Applied&nbsp;Job
                     </Link>
                   </li> 
                   <li className="nav-item" onClick={seekerLogout}>
@@ -128,21 +146,17 @@ if (userType === "admin") {
                   </li>
                 </ul>
               </div>
-            </div>
           </nav>
         </div>
-      </div>
     </>
   );
 } else if (userType === "recruiter") {
   return (
     <>
-      <div className="row navbar_main_row mb-1 sticky-top mb-1 ps-5 pe-4">
-        <div className="col-12">
-          <nav className="navbar navbar-expand-lg navbar-light">
-            <div className="container-fluid">
+      <div className="container-fluid navbar_main_row mb-1 px-0 sticky-top" ref={navRef}>
+          <nav className="navbar navbar-expand-lg navbar-light  bg-light p-0 px-3">
               <Link className="navbar-brand" to="/">
-              <img className='nav_bar_logo' src={`${API_URL}/upload/${data.logo}`} />
+              <img className='nav_bar_logo' src={`${API_URL}/upload/${data.img}`} />
             
               </Link>
               <button
@@ -166,17 +180,17 @@ if (userType === "admin") {
                  
                   <li className="nav-item">
                     <Link className="nav-link nav_font" aria-current="page" to="/recruiter/PostJob">
-                    <FaBriefcase />&nbsp;Job Post
+                    <FaBriefcase />&nbsp;Job&nbsp;Post
                     </Link>
                   </li>
                   <li className="nav-item">
                     <Link className="nav-link nav_font" aria-current="page" to="/recruiter/postedjob">
-                    <FaUser className="menu-icon" />&nbsp; postedJobs
+                    <FaUser className="menu-icon" />&nbsp;PostedJobs
                     </Link>
                   </li>
                   <li className="nav-item">
                     <Link className="nav-link nav_font" aria-current="page" to="/recruiter/appliedjob">
-                    <FaCheckCircle className="menu-icon" />&nbsp;Applied Job
+                    <FaCheckCircle className="menu-icon" />&nbsp;Applied&nbsp;Job
                     </Link>
                   </li>
                   
@@ -186,22 +200,18 @@ if (userType === "admin") {
                     </Link>
                   </li>
                 </ul>
-              </div>
-            </div>
+              </div>          
           </nav>
         </div>
-      </div>
     </>
   );
 } else {
   return (
     <>
-      <div className="row navbar_main_row sticky-top mb-1 ps-5 pe-4">
-        <div className="col-12">
-          <nav className="navbar navbar-expand-lg navbar-light">
-            <div className="container-fluid">
-              <Link className="navbar-brand" to="">
-                <img src="/logo.png" alt="logo" height={"70px"} />
+      <div className="container-fluid navbar_main_row sticky-top px-0" ref={navRef}>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light p-0 px-3">
+              <Link className="navbar-brand" to="/">
+                <img src="/logo.png" alt="logo" height={"45px"} />
               </Link>
               <button
                 className="navbar-toggler"
@@ -215,31 +225,31 @@ if (userType === "admin") {
                 <span className="navbar-toggler-icon" />
               </button>
               <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
+                <ul className="navbar-nav nav_font">
                   <li className="nav-item">
-                    <Link className="nav-link nav_font" aria-current="page" to="/">
+                    <Link className="nav-link pe-5" aria-current="page" to="/">
                     <FaHome />&nbsp;Home
                     </Link>
                   </li>
                   <li className="nav-item">
-                    <Link className="nav-link nav_font" to="/findjob">
-                    <FaBriefcase />&nbsp;Find a Job
+                    <Link className="nav-link pe-5" to="/findjob">
+                    <FaBriefcase />&nbsp;Find&nbsp;a&nbsp;Job
                     </Link>
                   </li>
                   <li className="nav-item">
-                    <Link className="nav-link nav_font" to="/about">
+                    <Link className="nav-link pe-5" to="/about">
                     <FaInfoCircle />&nbsp;About
                     </Link>
                   </li>
                   <li className="nav-item">
-                    <Link className="nav-link nav_font" to="/contact" tabIndex={-1} aria-disabled="true">
+                    <Link className="nav-link pe-5" to="/contact">
                     <FaPhoneAlt />&nbsp;Contact
                     </Link>
                   </li>
                 </ul>
-                <div className="dropdown ms-auto mt-2">
+                <div className="dropdown ms-auto me-2 mb-1">
                   <button
-                    className="btn btn-outline-secondary dropdown-toggle custom-btn"
+                    className="btn btn-outline-primary dropdown-toggle custom-btn p-1"
                     type="button"
                     id="dropdownMenuButton1"
                     data-bs-toggle="dropdown"
@@ -247,7 +257,7 @@ if (userType === "admin") {
                   >
                     <b>Registration</b>
                   </button>
-                  <ul className="dropdown-menu custom-dropdown" aria-labelledby="dropdownMenuButton1">
+                  <ul className="dropdown-menu custom-dropdown p-0" aria-labelledby="dropdownMenuButton1">
                     <li>
                       <Link className="dropdown-item" to="recruiter/register">
                       <FaUserPlus />&nbsp;Recruiter
@@ -255,14 +265,14 @@ if (userType === "admin") {
                     </li>
                     <li>
                       <Link className="dropdown-item" to="/seeker/register">
-                      <FaUserAlt />&nbsp;Job Seeker
+                      <FaUserAlt />&nbsp;Job&nbsp;Seeker
                       </Link>
                     </li>
                   </ul>
                 </div>
-                <div className="dropdown ms-4 mt-2 me-5">
+                <div className="dropdown me-2 mb-1">
                   <button
-                    className="btn btn-outline-primary dropdown-toggle custom-btn"
+                    className="btn btn-outline-primary dropdown-toggle custom-btn px-3 py-1"
                     type="button"
                     id="dropdownMenuButton2"
                     data-bs-toggle="dropdown"
@@ -270,28 +280,26 @@ if (userType === "admin") {
                   >
                     <b>Login</b>
                   </button>
-                  <ul className="dropdown-menu custom-dropdown" aria-labelledby="dropdownMenuButton2">
+                  <ul className="dropdown-menu custom-dropdown dropdown-menu-lg-end p-0" aria-labelledby="dropdownMenuButton2">
                     <li>
                       <Link className="dropdown-item" to="/admin/login">
-                      <FaUserShield />&nbsp;Admin Login
+                      <FaUserShield />&nbsp;Admin&nbsp;Login
                       </Link>
                     </li>
                     <li>
                       <Link className="dropdown-item" to="/recruiter/login">
-                      <FaUserTie />&nbsp;Recruiter Login
+                      <FaUserTie />&nbsp;Recruiter&nbsp;Login
                       </Link>
                     </li>
                     <li>
                       <Link className="dropdown-item" to="/seeker/login">
-                      <FaUser />&nbsp; Seeker Login
+                      <FaUser />&nbsp; Seeker&nbsp;Login
                       </Link>
                     </li>
                   </ul>
                 </div>
-              </div>
-            </div>
-          </nav>
-        </div>
+              </div>        
+          </nav>       
       </div>
     </>
   );
